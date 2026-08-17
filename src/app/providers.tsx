3a71b2b830be62c45271ff7094d8b5b8b826f315
tokkimi <goocciland@gmail.com>
2026-08-17 21:55:13 +0900

    Enable bilingual EasyAsso experience

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { SessionProvider } from 'next-auth/react';
+import { LanguageProvider } from '@/components/language-provider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider><LanguageProvider>{children}</LanguageProvider></SessionProvider>;
 }
